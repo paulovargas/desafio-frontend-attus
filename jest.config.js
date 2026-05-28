3ff58977f122b971164b04d023187c2fdb4a22f5
Paulo Vargas <paulotomegomesdevargas@gmail.com>
2026-05-28 18:14:00 -0300

    Configuração inicial do projeto

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+};
