222d49b8fb18e3c34855d8a8a26c06cdd3c40e3d
Paulo Vargas <paulotomegomesdevargas@gmail.com>
2026-05-29 20:03:51 -0300

    Criação do botão de cadastro de usuário

--- src/app/core/components/button-add/button-add.component.spec.ts
+++ src/app/core/components/button-add/button-add.component.spec.ts
@@ -1,7 +1,4 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonAddComponent } from './button-add.component';
 
@@ -9,12 +6,11 @@ describe('ButtonAddComponent', () => {
   let component: ButtonAddComponent;
   let fixture: ComponentFixture<ButtonAddComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ButtonAddComponent ]
-    })
-    .compileComponents();
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ButtonAddComponent]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ButtonAddComponent);
--- src/app/core/components/button-add/button-add.component.ts
+++ src/app/core/components/button-add/button-add.component.ts
@@ -2,18 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { UserFormDialogComponent } from '../../../users/feature-users/user-form-dialog/user-form-dialog/user-form-dialog.component';
 
 @Component({
   selector: 'app-button-add',
   templateUrl: './button-add.component.html',
   styleUrls: ['./button-add.component.css'],
-  imports: [MatButtonModule, MatDividerModule, MatIconModule]
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatDialogModule]
 })
 export class ButtonAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  openModal(): void {
+    this.dialog.open(UserFormDialogComponent, {
+      width: '580px',
+      maxWidth: 'calc(100vw - 48px)',
+      panelClass: 'user-form-dialog-panel',
+    });
   }
 
 }
